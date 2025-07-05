667f3a0d8920e0418e8e5e7fa9c59ecd2f88009a
NAVYASREE <navyareddylankapothu@gmail.com>
2025-07-05 15:45:20 +0530

    java fundamentals

---
+++ Java_fundamentals/src/Java_fundamentals/Question10.java
@@ -0,0 +1,22 @@
+package Java_fundamentals;
+
+import java.util.Scanner;
+
+public class Question10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = sc.next().charAt(0);
+
+        if (ch >= 'a' && ch <= 'z') {
+            ch = (char)(ch - 32);
+            System.out.println("Uppercase: " + ch);
+        } else if (ch >= 'A' && ch <= 'Z') {
+            ch = (char)(ch + 32);
+            System.out.println("Lowercase: " + ch);
+        } else {
+            System.out.println("Not an alphabet character");
+        }
+    }
+}
---
+++ Java_fundamentals/src/Java_fundamentals/Question4a.java
@@ -0,0 +1,19 @@
+package Java_fundamentals;
+
+public class Queston4a {
+
+	public static void main(String[] args) {
+	  int num = 56;
+	  if(num>0) {
+		  System.out.println("Positive");
+	  }
+	  else if(num<0) {
+		  System.out.println("Negative");
+	  }
+	  else {
+		  System.out.println("Zero");
+	  }
+
+	}
+
+}}
